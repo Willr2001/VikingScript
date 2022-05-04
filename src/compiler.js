f8a6fcd082bb44687edc72cd5b453cad5ecf7590
Ray Toal <rtoal@lmu.edu>
2022-05-04 15:34:24 -0700

    Bring in generator from Carlos, make basic programs work

--- src/compiler.js
+++ src/compiler.js
@@ -1,27 +1,27 @@
 import ast from "./ast.js"
 import analyze from "./analyzer.js"
-//import optimize from "./optimizer.js"
-//import generate from "./generator.js"
+import optimize from "./optimizer.js"
+import generate from "./generator.js"
 
 export default function compile(source, outputType) {
-  outputType = outputType.toLowerCase();
-  if (!["ast", "analyzed" /*, "optimized", "js"*/].includes(outputType)) {
+  outputType = outputType.toLowerCase()
+  if (!["ast", "analyzed", "optimized", "js"].includes(outputType)) {
     throw new Error("Unknown output type")
   }
   const program = ast(source)
-  if (outputType === "ast"){
+  if (outputType === "ast") {
     return program
   }
   const analyzed = analyze(program)
   if (outputType === "analyzed") {
     return analyzed
   }
-  /*
-  const optimized = optimize(analyzed)
+  const optimized = optimize(program)
   if (outputType === "optimized") {
     return optimized
   }
-  return generate(optimized)
-  */
+  const target = generate(program)
+  if (outputType === "js") {
+    return target
+  }
 }
-
